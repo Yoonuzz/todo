9e9df7505b715b35e3a3939b55987abd35c4844f
Yoonuzz <2001yoonus@gmail.com>
2022-07-08 14:17:47 +0530

    Todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GalleryComponent
+    GalleryComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
